8fd011808628d57bb8717eac5ea96a8ffbdb0943
YooHyeok <yjk7454@naver.com>
2024-03-19 17:44:53 +0900

    fix: Member 도메인 생상자중 1개를 기본생성자로 수정

--- src/main/java/springdb/jdbc/domain/Member.java
+++ src/main/java/springdb/jdbc/domain/Member.java
@@ -7,8 +7,7 @@ public class Member {
     private String memberId; // 회원아이디
     private int money; //소지 금액
 
-    public Member(String memberId) {
-        this.memberId = memberId;
+    public Member() {
     }
 
     public Member(String memberId, int money) {
